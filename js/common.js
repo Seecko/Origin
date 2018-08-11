4593ec20193a3336e29cd65174b156d349d52e8d
Seecko <qq603468769@126.com>
2018-08-11 17:35:34 +0800

    20180811
    Finish index,welcome,footer,sideBar(hover is not done),login(JS is not done),now I'm doing with html and css in reg on line 20.

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(function () {
+	init();
+});
+
+function init () {
+	$(".indexNav").load("sideBar.html .sideBarLoad",function(){
+		seajs.use("sideBar",function(sideJs) {
+			sideJs.inpEvt($(".search").find("input"),$(".icon-clear"));
+			sideJs.clrEvt($(".search").find("input"),$(".icon-clear"));
+		})
+		$(".indexContent").load("welcome.html .welcomeLoad",function(){
+			$(".indexFooter").load("footer.html .footerLoad",function() {
+				$("a").click(aClick);
+			});
+		});
+	});
+}
+//阻止a链接跳转并且在right加载链接里的页面
+//并且判断如果是点击的为注册、登陆页面，则不跳转，改为打开新页面
+//未完成,需要先清除之前页面的html,css和js,并且加载新页面的css，html和js
+function aClick(event) {
+	if (event.preventDefault) {
+		event.preventDefault();
+	} else{
+		window.event.returnValue=false;
+	}
+	if ($(this).parent().hasClass("login")) {
+		window.open($(this).attr("href"),'','height=700px,width=480px,top=0,left='+($(window).width()-480)/2+'px,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no');
+	}else{
+		$(".indexContent").load($(this).attr("href"));
+	}
+}
--- js/sideBar.js
+++ js/sideBar.js
@@ -1,42 +1,20 @@
-$(function () {
-	inpEvt($(".search").find("input"),$(".icon-clear"));
-	clrEvt($(".search").find("input"),$(".icon-clear"));
-});
-
-//	搜索框的事件
-//		搜索框输入出现删除图标
-function inpEvt (inpJqObj,clrJqObj) {
-	inpJqObj.keyup(function () {
-		if ($(this).val()!="") {
-			clrJqObj.css("visibility","visible");
-		}else{
-			clrJqObj.css("visibility","hidden");
-		}
-	});
-}
-//		点击删除图标清空文本框并且隐藏图标
-function clrEvt (inpJqObj,clrJqObj) {
-	clrJqObj.click(function () {
-		inpJqObj.val("");
-		clrJqObj.css("visibility","hidden");
-	});
-}
-//阻止a链接跳转并且在right加载链接里的页面
-//未完成,需要先清除之前页面的html,css和js,并且加载新页面的css，html和js
-$("a").click(function (event) {
-	if (event.preventDefault) {
-		event.preventDefault();
-	} else{
-		window.event.returnValue=false;
+define(function (require,exports,modules) {
+	//	搜索框的事件
+	//		搜索框输入出现删除图标
+	exports.inpEvt=function(inpJqObj,clrJqObj) {
+		inpJqObj.keyup(function () {
+			if ($(this).val()!="") {
+				clrJqObj.css("visibility","visible");
+			}else{
+				clrJqObj.css("visibility","hidden");
+			}
+		});
 	}
-	$(".indexContent").load($(this).attr("href"));
-})
-//				let sideBarCss=document.createElement("link");
-//				sideBarCss.rel="stylesheet";
-//				sideBarCss.type="text/css";
-//				sideBarCss.href="css/sideBar.css";
-//				$("head").append(sideBarCss);
-//				let sideBarJs=document.createElement("script");
-//				sideBarJs.type="text/javascript";
-//				sideBarJs.src="js/sideBar.js";
-//				$("html").append(sideBarJs);
+	//		点击删除图标清空文本框并且隐藏图标
+	exports.clrEvt=function(inpJqObj,clrJqObj) {
+		clrJqObj.click(function () {
+			inpJqObj.val("");
+			clrJqObj.css("visibility","hidden");
+		});
+	}
+});
